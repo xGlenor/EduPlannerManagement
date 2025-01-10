ef79d0381906acbf6f4cd514936ab12905ad888b
Grzegorz Duraj <grzegorzduraj2001@gmail.com>
2025-01-10 19:35:54 +0100

    Upload Project

---
+++ src/EduPlanner.Infrastructure/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using Keycloak.AuthServices.Authentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EduPlanner.Infrastructure;
+
+public static class Extensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+
+        services.AddKeycloakWebAppAuthentication(configuration);
+        services.AddAuthorization();
+        
+        return services;
+    }
+}
