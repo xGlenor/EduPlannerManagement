980b1ce9706ac5818b8026b617763458e2dc4754
Grzegorz Duraj <grzegorzduraj2001@gmail.com>
2025-01-11 06:03:57 +0100

    Added DbContext for MySQL

---
+++ src/EduPlanner.Infrastructure/Persistence/MySQL/MySQLDbContext.cs
@@ -0,0 +1,29 @@
+﻿using EduPlanner.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduPlanner.Infrastructure.Persistence.MySQL;
+
+public class MySQLDbContext: DbContext
+{
+    public MySQLDbContext(DbContextOptions<MySQLDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Group> Groups { get; set; }
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<GroupTree> CourseTrees { get; set; }
+    public DbSet<GroupCourse> GroupsCourses { get; set; }
+
+
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<GroupCourse>()
+            .HasKey(cg => new { cg.GroupId, cg.CourseId });
+        
+
+    }
+}
