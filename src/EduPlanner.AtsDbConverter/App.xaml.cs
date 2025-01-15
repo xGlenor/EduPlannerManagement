6dc29966eca4713d4ea36b6aa9b53b7b35001954
Grzegorz Duraj <grzegorzduraj2001@gmail.com>
2025-01-15 21:50:34 +0100

    Added support for MaterialDesign, removed StartupUri

--- src/EduPlanner.AtsDbConverter/App.xaml.cs
+++ src/EduPlanner.AtsDbConverter/App.xaml.cs
@@ -1,6 +1,13 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.Windows.Threading;
+using EduPlanner.AtsDbConverter.Views;
+using EduPlanner.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace EduPlanner.AtsDbConverter;
 
@@ -9,4 +16,47 @@ namespace EduPlanner.AtsDbConverter;
 /// </summary>
 public partial class App : System.Windows.Application
 {
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        using IHost host = CreateHostBuilder(args).Build();
+        host.Start();
+        
+        App app = new();
+        app.InitializeComponent();
+            
+            
+        app.MainWindow = host.Services.GetRequiredService<MainWindow>();
+        app.MainWindow.Visibility = Visibility.Visible;
+        app.Run();
+    }
+    
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostBuilderContext, configurationBuilder) =>
+            {
+            })
+            .ConfigureServices((hostContext, services) =>
+            {
+                //
+                // ADD VIEWS
+                //
+                services.AddSingleton<MainWindow>();
+                services.AddSingleton<MainView>();
+                services.AddSingleton<ConverterView>();
+                    
+                //
+                // ADD SERVICES
+                //
+
+
+                services.AddSingleton<Dispatcher>(_ => Current.Dispatcher);
+                    
+                services.AddDbContext<ApplicationDbContext>(opt =>
+                {
+                    var connectionString = hostContext.Configuration.GetConnectionString("Default");
+                    opt.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+                });
+            });
+    
 }
