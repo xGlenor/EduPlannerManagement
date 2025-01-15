2c365be9df6bf05d2146414644f00a555d485900
Grzegorz Duraj <grzegorzduraj2001@gmail.com>
2025-01-15 21:54:33 +0100

    Added ConverterView (Method "AddItem" to ListView, Method (ConvertDatabase), MainView (Method "CheckConnection" to check Mysql Connection, Method "CheckTextBoxes")

---
+++ src/EduPlanner.AtsDbConverter/Views/ConverterView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace EduPlanner.AtsDbConverter.Views;
+
+public partial class ConverterView : UserControl
+{
+    private readonly IServiceProvider _serviceProvider;
+    
+    public ConverterView(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+        InitializeComponent();
+    }
+
+    public void AddItem(string item)
+    {
+        ListInformation.Items.Add(item);
+    }
+    
+    private void ConvertDatabase(object sender, RoutedEventArgs e)
+    {
+        //var converterService = _serviceProvider.GetService<ConverterService>();
+        //converterService.Convert();
+    }
+}
---
+++ src/EduPlanner.AtsDbConverter/Views/MainView.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using EduPlanner.AtsDbConverter.Helpers;
+using Microsoft.Extensions.DependencyInjection;
+using MySqlConnector;
+
+namespace EduPlanner.AtsDbConverter.Views;
+
+public partial class MainView : UserControl
+{
+    private readonly IServiceProvider _serviceProvider;
+    
+    public MainView(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+        InitializeComponent();
+    }
+
+    private void CheckConnection(object sender, RoutedEventArgs e)
+    {
+        if (!CheckTextBoxes()) return;
+        
+        var service = _serviceProvider.GetService<MainWindow>();
+            
+        DbHelper mySqlHelper = new DbHelper(DbHost.Text,DbName.Text,DbLogin.Text,DbPassword.Password.ToString(),DbPort.Text);
+        if (!mySqlHelper.IsConnection) return;
+        service.ImportConfiguration.IsEnabled = true;
+        service.TabControl.SelectedIndex = 1;
+        DynamicConnectionString.Value = mySqlHelper.ConnectionString;;
+    }
+    
+    public bool CheckTextBoxes()
+    {
+        if (string.IsNullOrWhiteSpace(DbHost.Text) ||
+            string.IsNullOrWhiteSpace(DbName.Text) ||
+            string.IsNullOrWhiteSpace(DbLogin.Text) ||
+            string.IsNullOrWhiteSpace(DbPassword.Password) ||
+            string.IsNullOrWhiteSpace(DbPort.Text))
+        {
+            MessageBox.Show("Wypełnij wszystkie pola", "Wystąpił błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+            return false;
+        }
+
+        return true;
+    }
+
+}
