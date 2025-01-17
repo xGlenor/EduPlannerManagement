7a2f278f7bd24b4dc008ceac66f59bb5004d433e
Grzegorz Duraj <grzegorzduraj2001@gmail.com>
2025-01-17 23:40:23 +0100

    Changed type for StartDate and EndDate to TimeSpan

--- src/EduPlanner.Domain/Entities/Courses/CourseTime.cs
+++ src/EduPlanner.Domain/Entities/Courses/CourseTime.cs
@@ -10,23 +10,23 @@ public class CourseTime
 {
     [Column("idEvent")]
     public int CourseId { get; set; }
-    public Course Course { get; set; }
+    //public Course Course { get; set; }
     
     [Column("idWeek")]
     public int WeekId { get; set; }
-    public Week Week { get; set; }
+    //public Week Week { get; set; }
     
     [Column("idWeekDef")]
     public int WeekTypeId { get; set; }
-    public WeekType WeekType { get; set; }
+    //public WeekType WeekType { get; set; }
     
     [Column("idRoom")]
     public int? RoomId { get; set; }
-    public Room? Room { get; set; }
+    //public Room? Room { get; set; }
     
     [Column("dtStart")]
-    public DateTime? StartDate { get; set; }
+    public TimeSpan? StartDate { get; set; }
     [Column("dtStop")]
-    public DateTime? EndDate { get; set; }
+    public TimeSpan? EndDate { get; set; }
     
 }
