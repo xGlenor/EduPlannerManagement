16b8139896938767b2c6dc14a2ef36b0e6944ddf
Grzegorz Duraj <grzegorzduraj2001@gmail.com>
2025-01-22 10:21:31 +0100

    Added new parameter

--- src/EduPlanner.Application/Tree/TreeNodeDTO.cs
+++ src/EduPlanner.Application/Tree/TreeNodeDTO.cs
@@ -1,3 +1,3 @@
 namespace EduPlanner.Application.Tree;
 
-public record TreeNodeDTO<T>(int Id, string Name, bool? Show, IEnumerable<T> Groups);
+public record TreeNodeDTO<T>(int Id, string Name, bool? Show, IEnumerable<T> Groups, bool hasChildren);
