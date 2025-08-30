fe8c00d08f582434ad6e36e9ea329583163e2de8
KoruS <gsiwiec3131@gmail.com>
2025-08-30 19:59:51 +0200

    feat: add i18n config

---
+++ src/eduplanner-client/src/lib/i18n.ts
@@ -0,0 +1,19 @@
+﻿import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import {initReactI18next} from "react-i18next";
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    supportedLngs: ['pl', 'en'],
+    fallbackLng: 'pl',
+
+    debug: import.meta.env.DEV,
+
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n;
