33b56e77d320424c0f37888af75cfbbc26ed4bfd
KoruS <gsiwiec3131@gmail.com>
2025-09-03 23:40:17 +0200

    refactor: language support and move nav config

--- src/eduplanner-client/src/components/nav-header.tsx
+++ src/eduplanner-client/src/components/nav-header.tsx
@@ -5,33 +5,33 @@
   NavigationMenuList, NavigationMenuTrigger, navigationMenuTriggerStyle
 } from "@/components/ui/navigation-menu.tsx";
 import {Link} from "@tanstack/react-router";
+import {NAVIGATION_CONFIG} from "@/constants/navigationConfig.ts";
+import {useTranslation} from "react-i18next";
 
 export function NavHeader() {
+  const { t } = useTranslation();
+
   return (
     <NavigationMenu viewport={false}>
       <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-            <Link to="/">Strona główna</Link>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Plany zajęć</NavigationMenuTrigger>
-          <NavigationMenuContent className="whitespace-nowrap">
-            <NavigationMenuLink asChild>
-              <Link to="/">Grupy zajęciowe</Link>
-            </NavigationMenuLink>
-
-            <NavigationMenuLink asChild>
-              <Link to="/">Nauczyciele</Link>
-            </NavigationMenuLink>
-
-            <NavigationMenuLink asChild>
-              <Link to="/">Sale</Link>
-            </NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
+        {NAVIGATION_CONFIG.map(({key, i18nKey, children, ...props}) => !children ? (
+            <NavigationMenuItem key={key}>
+              <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                <Link {...props}>{t(i18nKey)}</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          ) : (
+            <NavigationMenuItem key={key}>
+              <NavigationMenuTrigger>{t(i18nKey)}</NavigationMenuTrigger>
+              <NavigationMenuContent className="whitespace-nowrap">
+                {children.map(({key, i18nKey, ...props}) => (
+                  <NavigationMenuLink key={key} asChild>
+                    <Link {...props}>{t(i18nKey)}</Link>
+                  </NavigationMenuLink>
+                ))}
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          ))}
       </NavigationMenuList>
     </NavigationMenu>
   )
---
+++ src/eduplanner-client/src/constants/navigationConfig.ts
@@ -0,0 +1,40 @@
+﻿import type {LinkProps} from "@tanstack/react-router";
+
+type NavItem = Omit<LinkProps, 'children'> & {
+  key: string;
+  i18nKey: string;
+  children?: NavItem[];
+}
+
+export const NAVIGATION_CONFIG: ReadonlyArray<NavItem> = [
+  {
+    key: "home",
+    to: "/",
+    i18nKey: "nav:home"
+  },
+  {
+    key: "schedules",
+    to: "/schedules/$resource",
+    i18nKey: "nav:schedules:label",
+    children: [
+      {
+        key: "schedules/class-group",
+        to: "/schedules/$resource",
+        params: { resource: 'class-groups' },
+        i18nKey: "nav:schedules:classGroups"
+      },
+      {
+        key: "schedules/teachers",
+        to: "/schedules/$resource",
+        params: { resource: 'teachers' },
+        i18nKey: "nav:schedules:teachers"
+      },
+      {
+        key: "schedules/rooms",
+        to: "/schedules/$resource",
+        params: { resource: 'rooms' },
+        i18nKey: "nav:schedules:rooms"
+      }
+    ]
+  }
+] as const;
