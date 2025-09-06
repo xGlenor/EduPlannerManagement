76a7a0fcae30549331f6043fbb100b41d32713f0
Grzegorz Duraj <grzegorzduraj2001@gmail.com>
2025-09-07 01:07:17 +0200

    Removed

--- src/EduPlanner.Application/Groups/GroupTreeDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EduPlanner.Application.Groups;
-
-public sealed record GroupTreeDto(
-    int IdGroupTree,
-    string Name,
-    List<GroupTreeDto> Children,
-    List<GroupDTO> Groups
-);
