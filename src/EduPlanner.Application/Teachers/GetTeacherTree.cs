fa9914221cbfe331b801314ce0e439c879146198
Grzegorz Duraj <grzegorzduraj2001@gmail.com>
2025-09-07 01:08:43 +0200

    Added new get tree endpoint for teachers

---
+++ src/EduPlanner.Application/Teachers/GetTeacherTree.cs
@@ -0,0 +1,7 @@
+﻿using EduPlanner.Application.Tree;
+using EduPlanner.Domain.Entities.Teachers;
+using MediatR;
+
+namespace EduPlanner.Application.Teachers;
+
+public sealed record GetTeacherTree(int? RootId = null) : IRequest<List<TreeDTO<TeacherDTO>>>;
